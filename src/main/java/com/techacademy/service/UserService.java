d44d86f44e4259859cbe385f84c6ef4a10c079bd
yotsuken <yotsumoto.ken@gmail.com>
2023-03-27 18:19:28 +0900

    3/27　USER登録コミット

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -1,6 +1,9 @@
 package com.techacademy.service;
 
 import java.util.List;
+
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
@@ -18,4 +21,13 @@ public class UserService {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
     }
+
+    // ----- 追加:ここから -----
+    /** Userの登録を行なう */
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+    // ----- 追加:ここまで -----
+
 }
